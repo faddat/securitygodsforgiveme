92f23cd386f9a747648425d73d786c740b69b791
Jacob Gadikian <faddat@gmail.com>
2015-06-30 14:13:25 +0700

    Update startup.sh
    Just playing.  Make sure you replace my public key with your own.

--- startup.sh
+++ startup.sh
@@ -12,8 +12,7 @@ chmod 600 /root/.ssh/id_rsa
 chmod 644 /root/.ssh/id_rsa.pub
 
 gsutil cp gs://fuckyeahchicken/sshd_config /etc/ssh/sshd_config
-printf "WHATEVER SSH KEY YOU PUT HERE BETWEEN THESE QUOTATION MARKS 
-WILL HAVE PASSWORDLESS ROOT LOGIN" > 
+printf "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCqHUGoqYnhwEI/8rQvh55+kVhGubVKONHcdR6QTv/RGTvneGwf6yTSx+mDWjITjWj7gNg3S+//U4SCtICUR/tN9SffZziZlfScBTGXW2gagOfLjX/ww/D6mGNMv5RT9Msk1zYoBK3/F5bfnwYGCQOvSgwbyWFDM2LZxYLuNGGMVT3y7Z95GT6dP1tKP21+6m47gBwppe2eMErpYWuZtDXY8FlHCxSPh4HH1PuHpo4f7STHXJIdrdN7aRjRB8yziCZ7mucWV+XxDhqpgyf13KznQRALnn5lMF84+h97nqVNrhKwZNuiOnCzk6JNYiUtjrpYFpt1UnCNKMtE0UEpzF5d7lyulS16cvfY3Vjl8hJ+lR5/KCqeyr/ObKGS4wyyFMm5+JjAOuwXijT+bDUVyT8WJIPqON9njHVxq9qyQ+LOurMLSCCRhdr6Sfv8vq1mWkXJixgtXy2nXpyAEKtCxWddWgLnWJS9QhFmdmdS9BR2ZxJZiLU/BdGtoM8etyraM6SbWm7870twr1BdL/RLCi14GfEIn4J4KGnsGi6nMVUPVCoiOWTy2E/2xcaO/weezMkjXsstp0xJa0r9n9PRkf64zqbzA4d4T3YYm9c6meiwFiofXTZFpYbXHuC1MSTZQ9/4u2CnrlDqBqHFIK23tINhFtSaGihfLtziFf0+iVdVrw== faddat@gmail.com" > 
 /root/.ssh/authorized_keys
 sudo service ssh restart
 print "okay all done"
